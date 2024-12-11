0852a7c675d9d887cb3c9b8dcf842e26c549902f
wangh5 <wangh5@m2600161857.v41.uthscsa.edu>
2024-12-11 13:42:37 -0600

    update __init__.py

--- ScanyCraft/enrich/__init__.py
+++ ScanyCraft/enrich/__init__.py
@@ -1,7 +1,13 @@
 from ._make_msigdb import  find_common_items, create_msigdb_df
 from ._get_deg import get_pairwise_deg
+from ._cheatmap import GSEAConfig, GSEAData, GSEAHeatmapPlotter, analyze_and_plot_gsea_results, plot_heatmap
 __all__ = [
     "find_common_items",
     "create_msigdb_df",
-    "get_pairwise_deg"
+    "get_pairwise_deg",
+    "GSEAConfig", 
+    "GSEAData",
+    "GSEAHeatmapPlotter", 
+    "analyze_and_plot_gsea_results",
+    "plot_heatmap"
 ]
--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='ScanyCraft',
     version='0.1',
+    description="A package for [your description here]",
+    author="Hanzhou Wang",
+    author_email="wangh5@uthscsa.edu",
+    url="https://github.com/whzemuch/ScanyCraft",
     packages=find_packages(),
     install_requires=[
         'numpy',        # Specify minimum version
