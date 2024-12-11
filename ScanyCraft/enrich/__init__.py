e8feae9b0ae04f4d06d6da8cea119031814b5e08
wangh5 <wangh5@m2600161857.v41.uthscsa.edu>
2024-12-11 13:48:35 -0600

    one more update init.py

--- ScanyCraft/enrich/__init__.py
+++ ScanyCraft/enrich/__init__.py
@@ -1,6 +1,6 @@
 from ._make_msigdb import  find_common_items, create_msigdb_df
 from ._get_deg import get_pairwise_deg
-from ._cheatmap import GSEAConfig, GSEAData, GSEAHeatmapPlotter, analyze_and_plot_gsea_results, plot_heatmap
+from ._cheatmap import GSEAConfig, GSEAData, GSEAHeatmapPlotter, analyze_and_plot_gsea_results
 __all__ = [
     "find_common_items",
     "create_msigdb_df",
@@ -8,6 +8,6 @@ __all__ = [
     "GSEAConfig", 
     "GSEAData",
     "GSEAHeatmapPlotter", 
-    "analyze_and_plot_gsea_results",
-    "plot_heatmap"
+    "analyze_and_plot_gsea_results"
+
 ]
